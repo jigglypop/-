d2c94b10f17174fb6a3f51968cd44c519dfef542
jigglypop <ydh2244@gmail.com>
2020-04-22 21:15:50 +0900

    0422

---
+++ 0420 - 복사본 - 복사본/다트게임.py
@@ -0,0 +1,16 @@
+import re
+
+letter = '1D2S*3T'
+
+bonus = {'S':1,'D':2,'T':3}
+option = {'':1,"*":2,'#':-1}
+
+
+p = re.compile('(\d+)([SDT])([*#]?)')
+dart = p.findall(letter)
+
+for i in range(len(dart)):
+    if dart[i][2] == '*' and i > 0:
+        dart[i-1] *= 2
+    dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+print(dart)
