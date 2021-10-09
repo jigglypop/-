bb151725657f29a0b74d4c4f3fa7ad72e199de36
jigglypop <ydh2244@gmail.com>
2021-10-09 18:08:50 +0900

    1009

--- 파이썬베이스/수학/소수/소수판별.py
+++ 파이썬베이스/수학/소수/소수판별.py
@@ -8,7 +8,7 @@ nums = list(map(int, input().split()))
 def isPrime(a):
     if a < 2:
         return False
-    for i in range(2, int(a ** 0.5)+1):
+    for i in range(2, int(a ** 0.5) + 1):
         if a % i == 0:
             return False
     return True
---
+++ 파이썬베이스/정규표현식/역참조/역참조_중복 copy.js
@@ -0,0 +1,2 @@
+let text = "1121112";
+console.log(text.match(/(\w)\1+|\w/g));
