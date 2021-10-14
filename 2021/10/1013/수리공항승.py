32463c6816f8768c6cda2f7afff369a131e29bc7
jigglypop <ydh2244@gmail.com>
2021-10-14 18:51:31 +0900

    1014

--- 2021/10/1013/수리공항승.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pprint import pprint
-from collections import deque
-sys.stdin = open("./text/1449.txt", "r")
-input = sys.stdin.readline
-N, L = map(int, input().split())
-nums = list(map(int, input().split()))
-print(nums)
