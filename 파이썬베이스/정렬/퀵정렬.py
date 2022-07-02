549efef77e546fd1e5c83ac09f121c58346b7ee6
jigglypop <ydh2244@gmail.com>
2022-07-02 09:39:00 +0900

    0702

--- 파이썬베이스/정렬/퀵정렬.py
+++ 파이썬베이스/정렬/퀵정렬.py
@@ -4,12 +4,9 @@ def quick(arr):
     pivot = arr[len(arr) // 2]
     L, M, H = [], [], []
     for num in arr:
-        if num < pivot:
-            L.append(num)
-        elif num > pivot:
-            H.append(num)
-        else:
-            M.append(num)
+        if num < pivot: L.append(num)
+        elif num > pivot: H.append(num)
+        else: M.append(num)
     return quick(L) + M + quick(H)
 
 print(quick([3, 1, 4, 5, 2]))
